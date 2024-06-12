e4a945f9f0bdf589683ea761ca979334dbcec239
tuandao <ngoctuan.dao@my.jcu.edu.au>
2024-06-12 17:26:12 +0800

    test 2

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 from chatbot.serializers import ConversationSerializer
-from  chatbot.models import Conversation
+from chatbot.models import Conversation
 from chatbot.simple_chat_bot import chat_bot
+
 
 # Create your views here.
 # Create CRUD for the Conversations model
@@ -13,13 +14,17 @@ class ListCreateConversation(generics.ListCreateAPIView):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
+
 list_create_conversation = ListCreateConversation.as_view()
+
 
 class RetrieveUpdateDestroyConversation(generics.RetrieveUpdateDestroyAPIView):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
+
 retrieve_update_destroy_conversation = RetrieveUpdateDestroyConversation.as_view()
+
 
 class AnswerMessage(generics.ListCreateAPIView):
     serializer_class = []
@@ -31,8 +36,8 @@ class AnswerMessage(generics.ListCreateAPIView):
             message = data.get("message")
             output_message = chat_bot(conversation_id, message)
             return Response({"AI message": output_message}, status=status.HTTP_200_OK)
-
-        except Exception as e:
+        except:
             return Response({"message": "failed"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 answer_message = AnswerMessage.as_view()
